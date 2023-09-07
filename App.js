4785161ef6827629b036f681d4befe823039d07c
VenkataHarshiniMannava <115424936+VenkataHarshiniMannava@users.noreply.github.com>
2023-09-07 12:02:48 +0530

    expense tracker

---
+++ App.js
@@ -0,0 +1,38 @@
+import { Fragment } from "react";
+import CompleteProfile from "./components/pages/profile/CompleteProfile";
+import AuthPage from "./components/pages/AuthPage/AuthPage";
+import { BrowserRouter as Router, Route ,Switch} from "react-router-dom";
+import CompleteProfileButtonWindow from "./components/pages/profile/CompleteProfileButtonWindow";
+import EmailVerification from "./components/pages/profile/EmailVerification";
+import ResetPassword from "./components/pages/profile/ResetPassword";
+import LandingPage from "./components/pages/landingPage/LandingPage";
+import ExpenseGenerator from "./components/pages/ExpenseFolder/ExpenseGenerator/ExpenseGenerator";
+import ExpenseFormContextProvider from "./contextStore/ExpenseFormContext/ExpenseFormContextProvider";
+
+
+function App() {
+  return (
+    <Router>
+      <ExpenseFormContextProvider>
+       
+      <Fragment >
+
+     
+        <Switch>
+        <Route exact path='/' component={LandingPage}/>
+        <Route exact path='/expensegenerator' component={ExpenseGenerator}/>
+        <Route exact path='/auth' component={AuthPage}/>
+        <Route exact path='/completeprofilebuttonwindow' component={CompleteProfileButtonWindow}/>
+        <Route exact path='/completeprofile' component={CompleteProfile}/>
+        <Route  exact path='/emailverification' component={EmailVerification}/>
+        <Route  exact path='/resetpassword' component={ResetPassword}/>
+        </Switch>
+      </Fragment>
+      
+      </ExpenseFormContextProvider>
+    </Router>
+
+  );
+}
+
+export default App;
